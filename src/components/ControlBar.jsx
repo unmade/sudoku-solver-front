d39341c16c611aec579dffaf9ab7d0b5d82c938c
Aleksei Maslakov <lesha.maslakov@gmail.com>
2020-02-03 23:48:42 +0300

    Hide control bar when sidebar is open

--- src/components/ControlBar.jsx
+++ src/components/ControlBar.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { Box, Layer } from 'grommet';
+import { Box, Layer, ThemeContext } from 'grommet';
 import HintButton from '../containers/HintButton';
 import History from '../containers/History';
 
 
+const zIndexLayer = {
+  layer: {
+    container: {
+      zIndex: 10,
+    },
+  },
+};
+
+
 const ControlBar = () => (
   <Box>
-    <Layer modal={false} position="bottom-left" responsive={false}>
-      <History />
-    </Layer>
-    <Layer modal={false} position="bottom-right" responsive={false}>
-      <Box pad="small">
-        <Box round="full" overflow="hidden" background="brand">
-          <HintButton />
+    <ThemeContext.Extend value={zIndexLayer}>
+      <Layer modal={false} position="bottom-left" responsive={false}>
+        <History />
+      </Layer>
+      <Layer modal={false} position="bottom-right" responsive={false}>
+        <Box pad="small">
+          <Box round="full" overflow="hidden" background="brand">
+            <HintButton />
+          </Box>
         </Box>
-      </Box>
-    </Layer>
+      </Layer>
+    </ThemeContext.Extend>
   </Box>
 );
 
